0692ff8fed3ddea34115cc71a5ad273abd1a3efa
Andrew Jack <me@andrewjack.uk>
2016-12-01 22:10:56 +0000

    Set upstream branch

--- npm-version/post-version.sh
+++ npm-version/post-version.sh
@@ -5,5 +5,5 @@ PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $
 git branch -m release/temp_$(git rev-parse --short HEAD^) release/$PACKAGE_VERSION
 
 # Push branch and tags
-git push origin release/$PACKAGE_VERSION
+git push --set-upstream origin release/$PACKAGE_VERSION
 git push origin --tags
